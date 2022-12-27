ece9b62a0f74657c9be1fb6c39274d427c5eeebc
Max Jimenez <nstyx999@protonmail.com>
2022-12-27 13:57:58 +0100

    Add character and animation rotation
    Added custom character and run, jump animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
-    public float moveSpeed = 3.5f;
-    public float jumpSpeed = 5f;
+    public float moveSpeed = 2.5f;
+    public float jumpSpeed = 3.8f;
 
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+
+    Vector3 moveDirection;
+    float turnSmooth = 15;
+
+    public Animator anim;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,12 +30,39 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         // movement
-        rb.velocity = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, verticalInput * moveSpeed);
-
+        //rb.velocity = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, verticalInput * moveSpeed);
+        Quaternion rotation = Quaternion.Euler(0, Mathf.Atan2(horizontalInput, verticalInput) * Mathf.Rad2Deg, 0);
+        
         // jump
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+            rb.velocity = new Vector3(horizontalInput * moveSpeed, jumpSpeed, verticalInput * moveSpeed);
         }
+        else
+        {
+            rb.velocity = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, verticalInput * moveSpeed);
+        }
+        //rb.rotation = Quaternion.LookRotation(rb.velocity);
+        //rb.rotation = Quaternion.LookRotation(rotation * Vector3.forward, Vector3.up); // to make the character stay vertical while jumping
+        rb.rotation = Quaternion.Slerp(rb.rotation, rotation, turnSmooth * Time.deltaTime);
+
+        anim.SetBool("isGrounded", IsGrounded());
+        anim.SetFloat("speed", (Mathf.Abs(verticalInput)) + (Mathf.Abs(horizontalInput)));
+
+                
+        /*if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("right");
+            //anim.SetBool("isMovingRight", true);
+            //anim.SetBool("IsMovingLeft", false);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("IsMovingLeft", true);
+        }*/
+    }
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, .1f, ground);
     }
 }
